c3bc4b3186f0a3d1138381250ca8071b084019b4
luoxi <2652445066@qq.com>
2022-01-04 23:10:58 +0800

    修改项目结构

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: luoxi
+ * @LastEditTime: 2022-01-03 21:11:07
+ * @LastEditors: your name
+ * @Description: app.vue
+-->
+<template>
+  <div id="app">
+    app
+    <Avatar :url="imgUrl" :size="100"/>
+  </div>
+</template>
+
+<script>
+
+import Avatar from "./components/Avatar.vue"
+export default {
+  name: "App",  //如果组件没有初始化名字，则会使用这个名字
+  components: {
+    Avatar
+  },
+  data(){
+      return{
+          imgUrl: "https://avatars.githubusercontent.com/u/53041755?v=4"
+      }
+  }
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: luoxi
+ * @LastEditTime: 2022-01-03 13:17:50
+ * @LastEditors: your name
+ * @Description: 头像组件
+-->
+<template>
+  <img class="avatar-img" :src="url" :style="imgStyle" alt="" />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+    },
+  },
+  data(){
+      return{
+          imgStyle: {
+              width:this.size+'px',
+              height:this.size+'px'
+          }
+      }
+  }
+};
+</script>
+
+<style scoped>
+/* 带有作用域的样式 */
+.avatar-img{
+    border-radius: 50%;
+    object-fit: cover;
+    display: block;
+}
+
+</style>
