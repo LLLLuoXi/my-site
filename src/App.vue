1ffee7c8f6dee8ab84ff2becede7168d07bb745a
luoxi <2652445066@qq.com>
2022-01-10 22:57:00 +0800

    新增icon组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!--
  * @Author: luoxi
- * @LastEditTime: 2022-01-03 21:11:07
+ * @LastEditTime: 2022-01-10 22:27:03
  * @LastEditors: your name
  * @Description: app.vue
 -->
@@ -8,16 +8,19 @@
   <div id="app">
     app
     <Avatar :url="imgUrl" :size="100"/>
+    <Icon type="close"/>
   </div>
 </template>
 
 <script>
 
 import Avatar from "./components/Avatar.vue"
+import Icon from "./components/Icon.vue"
 export default {
   name: "App",  //如果组件没有初始化名字，则会使用这个名字
   components: {
-    Avatar
+    Avatar,
+    Icon
   },
   data(){
       return{
---
+++ src/components/Icon.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: luoxi
+ * @LastEditTime: 2022-01-10 22:25:44
+ * @LastEditors: your name
+ * @Description: 图标组件
+-->
+<template>
+  <i class="iconfont icon-info" :class="fontClass"></i>
+</template>
+
+<script>
+const classMap = {
+  home: "icon-home-page",
+  success: "icon-zhengquewancheng-yuankuang",
+  error: "icon-cuowu",
+  close: "icon-guanbi",
+  warn: "icon-jinggao",
+  info: "icon-jinggao",
+  blog: "icon-blog",
+  code: "icon-code",
+  about: "icon-set_about_hov",
+  weixin: "icon-weixin",
+  mail: "icon-email",
+  github: "icon-github",
+  qq: "icon-QQ-circle-fill",
+  arrowUp: "icon-xiangshang",
+  arrowDown: "icon-xiangxia",
+  empty: "icon-empty",
+  chat: "icon-weixin",
+};
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    fontClass() {
+        return classMap[this.type]
+    },
+  },
+};
+</script>
+
+<style scope>
+@import "//at.alicdn.com/t/font_3131059_h97gaaxq9z8.css";
+</style>
