6a22031687d62e2f8ed3275e2107e33ca91a85d2
luoxi <2652445066@qq.com>
2022-01-14 23:36:13 +0800

    新增imageLoader组件

---
+++ src/components/ImageLoader/test.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: luoxi
+ * @LastEditTime: 2022-01-14 23:14:24
+ * @LastEditors: your name
+ * @Description: ImageLoader 测试页
+-->
+<template>
+  <div class="test-container">
+    <ImageLoader
+      src="http://source.unsplash.com/random/400x600"
+      placeholder="http://source.unsplash.com/random/100x100"
+      @load="handleLoaded"
+      :duration="3000"
+    />
+  </div>
+</template>
+
+<script>
+import ImageLoader from "./";
+export default {
+  components: {
+    ImageLoader,
+  },
+  methods: {
+    handleLoaded() {
+      console.log("ImageLoader组件load事件触发，图片加载完成");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.test-container {
+  width: 500px;
+  height: 400px;
+  border: 2px solid;
+  margin: 0 auto;
+}
+</style>
+
+
