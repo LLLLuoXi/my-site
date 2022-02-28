a68b70a6e2b76d5ccf0bb0e3850e8027a264c59a
luoxi <2652445066@qq.com>
2022-02-28 22:33:42 +0800

    ➕feat(*):新增nprogress进度条

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,63 @@
 /*
  * @Author: luoxi
- * @LastEditTime: 2022-02-27 22:29:18
+ * @LastEditTime: 2022-02-28 22:18:18
  * @LastEditors: your name
  * @Description: 路由配置 route
  */
+import "nprogress/nprogress.css"
+import { start, done, configure } from "nprogress"
+
+configure({
+    trickleSpeed: 20,
+    showSpinner: false,
+})
+function delay(duration) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, duration)
+    })
+
+}
+function getPageComponent(PageCompResolve) {
+    return async () => {
+        start()
+        if (process.env.NODE_ENV === 'development') {
+            await delay(1500)
+        }
+        const comp = await PageCompResolve()
+        done()
+        return comp
+    }
+}
 export default [
     // 路由规则
-    { name: 'Home', path: '/', component: () => import(/* webpackChunkName:"home" */"@/views/Home"), meta: { title: "首页" } },
-    { name: 'About', path: '/about', component: () => import(/* webpackChunkName:"about" */"@/views/About"), meta: { title: "关于我" } },
-    { name: 'Blog', path: '/blog', component: () => import(/* webpackChunkName:"blog" */"@/views/Blog"), meta: { title: "博客" } },
-    { name: 'CategoryBlog', path: '/blog/cate/:categoryId', component: () => import(/* webpackChunkName:"blog" */"@/views/Blog"), meta: { title: "文章" } },
-    { name: 'BlogDetail', path: '/blog/:id', component: () => import(/* webpackChunkName:"blogDetail" */"@/views/Blog/Detail"), meta: { title: "文章详情" } },
-    { name: 'Message', path: '/message', component: () => import(/* webpackChunkName:"message" */"@/views/Message"), meta: { title: "留言板" } },
-    { name: 'Project', path: '/project', component: () => import(/* webpackChunkName:"project" */"@/views/Project"), meta: { title: "项目&效果" } },
+    {
+        name: 'Home', path: '/', component: getPageComponent(() => import(/* webpackChunkName:"home" */"@/views/Home")),
+        meta: { title: "首页" }
+    },
+    {
+        name: 'About', path: '/about', component: getPageComponent(() => import(/* webpackChunkName:"about" */"@/views/About")),
+        meta: { title: "关于我" }
+    },
+    {
+        name: 'Blog', path: '/blog', component: getPageComponent(() => import(/* webpackChunkName:"blog" */"@/views/Blog")),
+        meta: { title: "博客" }
+    },
+    {
+        name: 'CategoryBlog', path: '/blog/cate/:categoryId', component: getPageComponent(() => import(/* webpackChunkName:"blog" */"@/views/Blog")),
+        meta: { title: "文章" }
+    },
+    {
+        name: 'BlogDetail', path: '/blog/:id', component: getPageComponent(() => import(/* webpackChunkName:"blogDetail" */"@/views/Blog/Detail")),
+        meta: { title: "文章详情" }
+    },
+    {
+        name: 'Message', path: '/message', component: getPageComponent(() => import(/* webpackChunkName:"message" */"@/views/Message")),
+        meta: { title: "留言板" }
+    },
+    {
+        name: 'Project', path: '/project', component: getPageComponent(() => import(/* webpackChunkName:"project" */"@/views/Project")),
+        meta: { title: "项目&效果" }
+    },
 ]
